09d982967537e3ebf23ba2b1a2465954b383ef3a
ChaoShen0 <44055265+ChaoShen0@users.noreply.github.com>
2018-11-19 07:48:17 +0000

    add approximate solution to inf ball bouncing

--- CGcommon/Physics.h
+++ CGcommon/Physics.h
@@ -1,5 +1,6 @@
 #pragma once
-
+#include <iostream>
+using namespace std;
 #include <stdlib.h>
 #include "CGobject.h"
 
@@ -7,8 +8,9 @@ namespace Physics
 {
 	// Physics
 	bool gravity = true;
-//	float coef = 0.8f;  // coefficient of restitution
-
+	//	float coef = 0.8f;  // coefficient of restitution
+	float lastcollisionV[3] = { 0.0f };
+	bool staticsflag = false;
 	float forces(int j)
 	{
 		if (!gravity)
@@ -25,26 +27,48 @@ namespace Physics
 		{
 			if (sceneObject->translateVector[i] >= 1.0)
 			{
+				
 				sceneObject->velocity[i] = -sceneObject->coef * sceneObject->velocity[i];
 				sceneObject->translateVector[i] = 1.0 - sceneObject->coef * (sceneObject->translateVector[i] - 1.0);
+				
+			
 			}
 
 			if (sceneObject->translateVector[i] <= -1.0)
 			{
-				sceneObject->velocity[i] = -sceneObject->coef * sceneObject->velocity[i];
-				sceneObject->translateVector[i] = -1.0 - sceneObject->coef * (sceneObject->translateVector[i] + 1.0);
+				
+				if (fabs(fabs(sceneObject->velocity[1]) - fabs(-sceneObject->coef * sceneObject->velocity[1])) > 0.06) 
+				{
+					sceneObject->velocity[i] = -sceneObject->coef * sceneObject->velocity[i];
+					sceneObject->translateVector[i] = -1.0 ;
+					cout << sceneObject->velocity[i] << endl;
+
+				}
+				else
+				{
+					sceneObject->velocity[i] = 0;
+					sceneObject->translateVector[i] = -1.0;
+					//gravity = false;
+				}
+			
 			}
+			lastcollisionV[i] = sceneObject->velocity[i];
 		}
 	}
 
 	void updatePhysics(float deltaTime, CGCommon::CGObject* sceneObject)
 	{
-		for (int j = 0; j < 3; j++)  // x, y, z directions
+		if (sceneObject->translateVector[1] <= -1.0 && fabs(fabs(sceneObject->velocity[1]) - fabs(-sceneObject->coef * sceneObject->velocity[1])) < 0.02)
 		{
-			sceneObject->translateVector[j] += deltaTime * sceneObject->velocity[j];
-			sceneObject->velocity[j] += deltaTime * forces(j) / sceneObject->mass;
 		}
-
+		else
+		{
+			for (int j = 0; j < 3; j++)  // x, y, z directions
+			{
+				sceneObject->translateVector[j] += deltaTime * sceneObject->velocity[j];
+				sceneObject->velocity[j] += deltaTime * forces(j) / sceneObject->mass;
+			}
+		}
 		collision(sceneObject);
 	}
 }
