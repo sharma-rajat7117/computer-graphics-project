8ca39c0725c718d81ec98a10b2edb792f1929b98
Marcus Sung <sungm@ITSERV.SCSS.TCD.IE>
2018-11-30 23:22:35 +0000

    tree particles

--- CGcommon/main.cpp
+++ CGcommon/main.cpp
@@ -66,6 +66,7 @@ const unsigned int SCR_HEIGHT = 1100;
 GLuint programID;
 GLuint lightingID;
 GLuint waterParticleID;
+GLuint leafParticleID;
 //GLuint flagID;
 
 // Shader attribute locations
@@ -98,6 +99,7 @@ GLuint lightVAO;
 GLuint treeVAO;
 GLuint flagpostVAO;
 GLuint waterParticleVAO;
+GLuint leafParticleVAO;
 GLuint flagVAO;
 
 // ---- water particles vbo --- //
@@ -142,7 +144,7 @@ float deltaTime = 0.0f;	// time between current frame and last frame
 float lastFrame = 0.0f;
 
 //lighting position
-glm::vec3 lightPos(1.0f, 1.0f, 3.0f);
+glm::vec3 lightPos(1.0f, 5.0f, 3.0f);
 
 bool rotateCubes = false;
 
@@ -253,6 +255,7 @@ void initialiseWaterParticles2() {
 }
 
 
+
 //GLuint Texture = loadDDS("particle.DDS");
 
 void initialiseWaterTexture() {
@@ -289,6 +292,62 @@ void initialiseWaterTexture() {
 
 //---------------------------------------------------------------------------------------------------------//
 
+
+//-----------------------------------Leaf Particles----------------------------------------------------//
+
+struct leafParticle {
+	glm::vec3 pos, speed;
+	unsigned char r, g, b, a; // Color
+	float size, angle, weight;
+	float life; // Remaining life of the particle. if <0 : dead and unused.
+	float cameradistance; // *Squared* distance to the camera. if dead : -1.0f
+
+	bool operator<(const leafParticle& that) const {
+		// Sort in reverse order : far particles drawn first.
+		return this->cameradistance > that.cameradistance;
+	}
+};
+const int leafMaxParticles = 50000;
+leafParticle leafParticlesContainer[leafMaxParticles];
+int LastUsedLeafParticle = 0;
+
+// Finds a Particle in ParticlesContainer which isn't used yet.
+// (i.e. life < 0);
+int FindUnusedLeafParticle() {
+
+	for (int i = LastUsedLeafParticle; i<leafMaxParticles; i++) {
+		if (leafParticlesContainer[i].life < 0) {
+			LastUsedParticle = i;
+			return i;
+		}
+	}
+
+	for (int i = 0; i<LastUsedLeafParticle; i++) {
+		if (leafParticlesContainer[i].life < 0) {
+			LastUsedLeafParticle = i;
+			return i;
+		}
+	}
+
+	return 0; // All particles are taken, override the first one
+}
+
+void SortLeafParticles() {
+	std::sort(&leafParticlesContainer[0], &leafParticlesContainer[leafMaxParticles]);
+}
+
+void initialiseLeafParticles() {
+	g_particule_position_size_data = new GLfloat[leafMaxParticles * 4];
+	g_particule_color_data = new GLubyte[leafMaxParticles * 4];
+
+	for (int i = 0; i < MaxParticles; i++) {
+		leafParticlesContainer[i].life = -1.0f;
+		leafParticlesContainer[i].cameradistance = -1.0f;
+	}
+}
+
+
+//-----------------------------------------------------------------------------------------------------------//
 void bindVertexAttribute(int location, int locationSize, int startVBO, int offsetVBO)
 {
 	glEnableVertexAttribArray(location);
@@ -507,6 +566,7 @@ void createShaders()
 	programID = LoadShaders("../CGcommon/shaders/shader.vs", "../CGcommon/shaders/shader.fs");
 	lightingID = LoadShaders("../CGcommon/shaders/lighting.vs", "../CGcommon/shaders/lighting.fs");
 	waterParticleID = LoadShaders("../CGcommon/shaders/Particle.vertexshader", "../CGcommon/shaders/Particle.fragmentshader");
+	leafParticleID = LoadShaders("../CGcommon/shaders/Particle.vertexshader", "../CGcommon/shaders/Particle.fragmentshader");
 	//	flagID = LoadShaders("../CGcommon/shaders/flag.vs", "../CGcommon/shaders/flag.fs");
 }
 
@@ -818,9 +878,9 @@ void createObjects()
 	footballb2.startIBO = footballb3.startIBO = footballb.startIBO;
 
 	// tree
-	const char* treeFileName = "../CGCommon/meshes/DeadTree/DeadTree.obj";
+	const char* treeFileName = "../CGCommon/meshes/Tree/tree03.obj";
 	vector<objl::Mesh> treemeshes = loadMeshes(treeFileName);
-	tree = loadObjObject(treemeshes[0], true, treeVAO, false, vec3(-0.75f, -0.25f, 0.0f), vec3(0.1f, 0.2f, 0.1f), vec3(0.139f, 0.69f, 0.19f), 0.0f, NULL);
+	tree = loadObjObject(treemeshes[0], true, treeVAO, false, vec3(-0.75f, -0.25f, 0.0f), vec3(1.0f, 1.0f, 1.0f), vec3(0.139f, 0.69f, 0.19f), 0.0f, NULL);
 
 	// add flagpost
 	const char* flagPostFileName = "../CGCommon/meshes/Cylinder/cylinder.obj";
@@ -930,6 +990,8 @@ void createObjects()
 	delete flagIndices;
 }
 
+
+
 void init()
 {
 	glEnable(GL_DEPTH_TEST);
@@ -940,6 +1002,8 @@ void init()
 	initialiseWaterParticles();
 
 	initialiseWaterParticles2();
+
+	initialiseLeafParticles();
 
 	// Vertex shader
 	CameraRight_worldspace_ID = glGetUniformLocation(waterParticleID, "CameraRight_worldspace");
@@ -954,6 +1018,202 @@ void init()
 	setupUniformVariables();
 
 	createObjects();
+}
+
+
+
+void leafParticlesStart() {
+
+	// Use water shader
+	glUseProgram(waterParticleID);
+
+
+	// Generate 10 new particule each millisecond,
+	// but limit this to 16 ms (60 fps), or if you have 1 long frame (1sec),
+	// newparticles will be huge and the next frame even longer.
+	int newparticles = (int)(deltaTime*1000.0);
+	if (newparticles > (int)(0.016f*1000.0))
+		newparticles = (int)(0.016f*1000.0);
+
+	for (int i = 0; i < newparticles; i++) {
+		int particleIndex = FindUnusedParticle();
+		leafParticlesContainer[particleIndex].life = 5.0f; // This particle will live n seconds.
+		leafParticlesContainer[particleIndex].pos = glm::vec3(0, -1.30f, -8.0f);
+
+		float spread = 0.2f;
+		glm::vec3 maindir = glm::vec3(0.0f, -1.0f, 0.0f);
+		// Very bad way to generate a random direction; 
+		// See for instance http://stackoverflow.com/questions/5408276/python-uniform-spherical-distribution instead,
+		// combined with some user-controlled parameters (main direction, spread, etc)
+		glm::vec3 randomdir = glm::vec3(
+			(rand() % 2000 - 1000.0f) / 1000.0f,
+			(rand() % 2000 - 1000.0f) / 1000.0f,
+			(rand() % 2000 - 1000.0f) / 1000.0f
+		);
+
+		leafParticlesContainer[particleIndex].speed = maindir + randomdir * spread;
+
+
+		// Very bad way to generate a random color  green : rgba(30, 130, 76, 1)
+		leafParticlesContainer[particleIndex].r = 30;//rand() % 256;
+		leafParticlesContainer[particleIndex].g = 130; //rand() % 256;
+		leafParticlesContainer[particleIndex].b = 76;// rand() % 256;
+		leafParticlesContainer[particleIndex].a = (rand() % 256) ;
+
+		leafParticlesContainer[particleIndex].size =  0.05f;
+
+	}
+
+	// Simulate all particles
+	int ParticlesCount = 0;
+	for (int i = 0; i < leafMaxParticles; i++) {
+
+		leafParticle& p = leafParticlesContainer[i]; // shortcut
+
+		if (p.life > 0.0f) {
+			//printf("%.6f", Physics::forces(1));
+			// Decrease life
+			p.life -= deltaTime;
+			if (p.life > 0.0f) {
+
+				// Simulate simple physics : gravity only, no collisions
+				p.speed += glm::vec3(0.0f, -9.8, 0.0f) * (float)deltaTime * 0.005f;
+			
+				
+
+				p.pos += p.speed * (float)deltaTime ;
+				p.pos.y += p.pos.y / (2.0f*1000.0f);
+				//p.pos += 
+				p.cameradistance = glm::length(p.pos - cameraPos);
+				//ParticlesContainer[i].pos += glm::vec3(0.0f,10.0f, 0.0f) * (float)delta;
+
+				// Fill the GPU buffer
+				g_particule_position_size_data[4 * ParticlesCount + 0] = p.pos.x;
+				g_particule_position_size_data[4 * ParticlesCount + 1] = p.pos.y;
+				g_particule_position_size_data[4 * ParticlesCount + 2] = p.pos.z;
+
+				g_particule_position_size_data[4 * ParticlesCount + 3] = p.size;
+
+				g_particule_color_data[4 * ParticlesCount + 0] = p.r;
+				g_particule_color_data[4 * ParticlesCount + 1] = p.g;
+				g_particule_color_data[4 * ParticlesCount + 2] = p.b;
+				g_particule_color_data[4 * ParticlesCount + 3] = p.a;
+
+			}
+			else {
+				// Particles that just died will be put at the end of the buffer in SortParticles();
+				p.cameradistance = -1.0f;
+			}
+
+			ParticlesCount++;
+
+		}
+	}
+
+	SortParticles();
+
+	//printf("%d ", ParticlesCount);
+
+
+	// Update the buffers that OpenGL uses for rendering.
+	// There are much more sophisticated means to stream data from the CPU to the GPU, 
+	// but this is outside the scope of this tutorial.
+	// http://www.opengl.org/wiki/Buffer_Object_Streaming
+
+
+	glBindBuffer(GL_ARRAY_BUFFER, particles_position_buffer);
+	glBufferData(GL_ARRAY_BUFFER, MaxParticles * 4 * sizeof(GLfloat), NULL, GL_STREAM_DRAW); // Buffer orphaning, a common way to improve streaming perf. See above link for details.
+	glBufferSubData(GL_ARRAY_BUFFER, 0, ParticlesCount * sizeof(GLfloat) * 4, g_particule_position_size_data);
+
+	glBindBuffer(GL_ARRAY_BUFFER, particles_color_buffer);
+	glBufferData(GL_ARRAY_BUFFER, MaxParticles * 4 * sizeof(GLubyte), NULL, GL_STREAM_DRAW); // Buffer orphaning, a common way to improve streaming perf. See above link for details.
+	glBufferSubData(GL_ARRAY_BUFFER, 0, ParticlesCount * sizeof(GLubyte) * 4, g_particule_color_data);
+
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+
+
+
+	// -------------------------- TEXTURE --------------------------
+
+	glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+
+	// Bind our texture in Texture Unit 0
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	// Set our "myTextureSampler" sampler to use Texture Unit 0
+	glUniform1i(TextureID, 0);
+	// Same as the billboards tutorial
+
+	glm::mat4 projectionfountain = glm::perspective(glm::radians(fov), (float)(SCR_WIDTH) / (float)(SCR_HEIGHT), 0.1f, 100.0f);
+	glUniformMatrix4fv(projection_waterparticle, 1, GL_FALSE, &projectionfountain[0][0]);
+
+	// camera/view transformation
+	glUniformMatrix4fv(view_waterparticle, 1, GL_FALSE, &view[0][0]);
+	glm::vec3 leavesPos(-4.2f, 5.0f, 6.0f);
+	//root camera
+	glm::mat4 local_leaves(1.0f);
+	local_leaves = glm::translate(local_leaves, leavesPos);
+	glm::mat4 glob_leaves = local_leaves;
+	glUniformMatrix4fv(model_waterparticle, 1, GL_FALSE, &glob_leaves[0][0]);
+
+	glUniform3f(CameraRight_worldspace_ID, view[0][0], view[1][0], view[2][0]);
+	glUniform3f(CameraUp_worldspace_ID, view[0][1], view[1][1], view[2][1]);
+	glUniformMatrix4fv(ViewProjMatrixID, 1, GL_FALSE, &projectionfountain[0][0]);
+
+
+	// 1rst attribute buffer : vertices
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, billboard_vertex_buffer);
+	glVertexAttribPointer(
+		0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
+		3,                  // size
+		GL_FLOAT,           // type
+		GL_FALSE,           // normalized?
+		0,                  // stride
+		(void*)0            // array buffer offset
+	);
+
+	// 2nd attribute buffer : positions of particles' centers
+	glEnableVertexAttribArray(1);
+	glBindBuffer(GL_ARRAY_BUFFER, particles_position_buffer);
+	glVertexAttribPointer(
+		1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+		4,                                // size : x + y + z + size => 4
+		GL_FLOAT,                         // type
+		GL_FALSE,                         // normalized?
+		0,                                // stride
+		(void*)0                          // array buffer offset
+	);
+
+	// 3rd attribute buffer : particles' colors
+	glEnableVertexAttribArray(2);
+	glBindBuffer(GL_ARRAY_BUFFER, particles_color_buffer);
+	glVertexAttribPointer(
+		2,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+		4,                                // size : r + g + b + a => 4
+		GL_UNSIGNED_BYTE,                 // type
+		GL_TRUE,                          // normalized?    *** YES, this means that the unsigned char[4] will be accessible with a vec4 (floats) in the shader ***
+		0,                                // stride
+		(void*)0                          // array buffer offset
+	);
+
+	// These functions are specific to glDrawArrays*Instanced*.
+	// The first parameter is the attribute buffer we're talking about.
+	// The second parameter is the "rate at which generic vertex attributes advance when rendering multiple instances"
+	// http://www.opengl.org/sdk/docs/man/xhtml/glVertexAttribDivisor.xml
+	glVertexAttribDivisor(0, 0); // particles vertices : always reuse the same 4 vertices -> 0
+	glVertexAttribDivisor(1, 1); // positions : one per quad (its center)                 -> 1
+	glVertexAttribDivisor(2, 1); // color : one per quad                                  -> 1
+
+								 // Draw the particules !
+								 // This draws many times a small triangle_strip (which looks like a quad).
+								 // This is equivalent to :
+								 // for(i in ParticlesCount) : glDrawArrays(GL_TRIANGLE_STRIP, 0, 4), 
+								 // but faster.
+	glDrawArraysInstanced(GL_TRIANGLE_STRIP, 0, 4, ParticlesCount);
 }
 
 void display()
@@ -1115,7 +1375,7 @@ void display()
 	int ParticlesCount = 0;
 	for (int i = 0; i < MaxParticles; i++) {
 
-		WaterParticle& p = ParticlesContainer[i]; // shortcut
+			WaterParticle& p = ParticlesContainer[i]; // shortcut
 
 		if (p.life > 0.0f) {
 
@@ -1492,10 +1752,14 @@ void display()
 	glDisableVertexAttribArray(2);
 
 
+	///-------------------leaves---------------------//
 
+	leafParticlesStart();
+	//-----------------------------------------------//
 	// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 	glfwSwapBuffers(window);
 	glfwPollEvents();
+
 }
 
 int main(void) {
